a6be02d472d780efafe5cb05ac5abc00bd728dde
mohitDora <doramohitkumar@gmail.com>
2022-06-02 19:43:47 +0530

    comiteed

--- src/App2.js
+++ src/App2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Card2 from "./Components/Card2";
-import "./index.css";
+import "./index1.css";
 // import Navgame from "./Components/Navgame"
 
 export default function App2(){
